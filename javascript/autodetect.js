c99aedb200a8aa3dec510594516dcb1281e0b59a
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2011-05-27 15:29:35 -0400

    Added x and y stepping algorithms

--- javascript/autodetect.js
+++ javascript/autodetect.js
@@ -402,127 +402,21 @@ function launchTestWindow()
 }
 
 /**
- * Automatic curve extraction algorithm.
+ * Select which auto extraction curve is to be triggered.
  */
 function scanPlot()
 {
-  closePopup("testImageWindow");
-  /* This is only a brute forced algorithm */
-  var xPoints = new Array();
-  var xPointsPicked = 0;
-  xyData = [];
-  pointsPicked = 0;
-  
-  redrawCanvas();
-  markedScreen = currentScreen;
-  
-  var xStepEl = document.getElementById("xStep");
-  var xStep = parseFloat(xStepEl.value);
-  var yStepEl = document.getElementById("yStep");
-  var yStep = parseFloat(yStepEl.value);
-  
-  var dw = canvasWidth;
-  var dh = canvasHeight;
-  
-  var blobAvg = new Array();
-  
-  for(var coli = 0; coli < dw; coli++)
-  {
-    blobs = -1;
-    firstbloby = -2.0*yStep;
-    bi = 0;
-       
-    for(var rowi = 0; rowi < dh; rowi++)
-    {
-	if (binaryData[rowi][coli] == true)
-	{
-	  if (rowi > firstbloby + yStep)
-	  {
-	    blobs = blobs + 1;
-	    bi = 1;
-	    blobAvg[blobs] = rowi;
-	    firstbloby = rowi;
-	  }
-	  else
-	  {
-	    bi = bi + 1;
-	    blobAvg[blobs] = parseFloat((blobAvg[blobs]*(bi-1.0) + rowi)/parseFloat(bi));
-	  }
-	}
-	
-    }
-    if (blobs >= 0)
-    {
-	xi = coli;
-	for (var blbi = 0; blbi <= blobs; blbi++)
-	{
-	  yi = blobAvg[blbi];
-	  
-	  xPoints[xPointsPicked] = new Array();
-	  xPoints[xPointsPicked][0] = parseFloat(xi);
-	  xPoints[xPointsPicked][1] = parseFloat(yi);
-	  xPoints[xPointsPicked][2] = 1; // 1 if not filtered, 0 if processed already
-	  xPointsPicked = xPointsPicked + 1;
-	}
-    }
     
-  }
-  
-  if (xPointsPicked == 0)
-    return 0;
-  
-  for(var pi = 0; pi < xPointsPicked; pi++)
-  {
-    if(xPoints[pi][2] == 1) // if still available
-    {
-      var inRange = 1;
-      var xxi = pi+1;
-      
-      var oldX = xPoints[pi][0];
-      var oldY = xPoints[pi][1];
-      
-      var avgX = oldX;
-      var avgY = oldY;
-      
-      var matches = 1;
-      
-      while((inRange == 1) && (xxi < xPointsPicked))
-      {
-	    var newX = xPoints[xxi][0];
-	    var newY = xPoints[xxi][1];
-	
-	    if( (Math.abs(newX-oldX) <= xStep) && (Math.abs(newY-oldY) <= yStep) && (xPoints[xxi][2] == 1))
-	    {
-	      avgX = (avgX*matches + newX)/(matches+1.0);
-	      avgY = (avgY*matches + newY)/(matches+1.0);
-	      matches = matches + 1;
-	      
-	      xPoints[xxi][2] = 0;
-	    }
-	    if (newX > oldX + 2*xStep)
-	      inRange = 0;
-	
-	    xxi = xxi + 1;
-      }
-      
-      xPoints[pi][2] = 0; 
-      
-      xyData[pointsPicked] = new Array();
-      xyData[pointsPicked][0] = parseFloat(avgX);
-      xyData[pointsPicked][1] = parseFloat(avgY);
-      pointsPicked = pointsPicked + 1;	
-  
-      ctx.beginPath();
-      ctx.fillStyle = "rgb(200,0,200)";
-      ctx.arc(parseInt(avgX),parseInt(avgY),3,0,2.0*Math.PI,true);
-      ctx.fill();
-
-      
-    }
+    autoStepEl = document.getElementById('autostepalgo');
+    xStepEl = document.getElementById('xstepalgo');
+    yStepEl = document.getElementById('ystepalgo');
     
-  }
-  xPoints = [];	
-  pointsStatus(pointsPicked);  
-  return pointsPicked;
+    if(autoStepEl.checked == true)
+        AE_averagingWindow();
+    else if(xStepEl.checked == true)
+        AE_xstep();
+    else if(yStepEl.checked == true)
+        AE_ystep();
+        
 }
 
