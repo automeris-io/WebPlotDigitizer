20d282feea0de2fa96b6ee802b3cec8e6f2dfc7b
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2011-05-27 15:31:37 -0400

    updated index.html and combined.js

--- combined.js
+++ combined.js
@@ -650,9 +650,60 @@ function launchTestWindow()
 }
 
 /**
- * Automatic curve extraction algorithm.
+ * Select which auto extraction curve is to be triggered.
  */
 function scanPlot()
+{
+    
+    autoStepEl = document.getElementById('autostepalgo');
+    xStepEl = document.getElementById('xstepalgo');
+    yStepEl = document.getElementById('ystepalgo');
+    
+    if(autoStepEl.checked == true)
+        AE_averagingWindow();
+    else if(xStepEl.checked == true)
+        AE_xstep();
+    else if(yStepEl.checked == true)
+        AE_ystep();
+        
+}
+
+/*
+	WebPlotDigitizer - http://arohatgi.info/WebPlotDigitizer
+
+	Version 2.3
+
+	Copyright 2010 Ankit Rohatgi <ankitrohatgi@hotmail.com>
+
+	This file is part of WebPlotDigitizer.
+
+    WebPlotDigitizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebPlotDigitizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+/**
+ * @fileoverview Automatic extraction algorithms.
+ * @version 2.3
+ * @author Ankit Rohatgi ankitrohatgi@hotmail.com
+ */
+ 
+ 
+/**
+ * Automatic curve extraction algorithm using an averaging window.
+ */
+function AE_averagingWindow()
 {
   closePopup("testImageWindow");
   /* This is only a brute forced algorithm */
@@ -699,19 +750,20 @@ function scanPlot()
 	}
 	
     }
+    
     if (blobs >= 0)
     {
-	xi = coli;
-	for (var blbi = 0; blbi <= blobs; blbi++)
-	{
-	  yi = blobAvg[blbi];
-	  
-	  xPoints[xPointsPicked] = new Array();
-	  xPoints[xPointsPicked][0] = parseFloat(xi);
-	  xPoints[xPointsPicked][1] = parseFloat(yi);
-	  xPoints[xPointsPicked][2] = 1; // 1 if not filtered, 0 if processed already
-	  xPointsPicked = xPointsPicked + 1;
-	}
+	    xi = coli;
+	    for (var blbi = 0; blbi <= blobs; blbi++)
+	    {
+	      yi = blobAvg[blbi];
+	      
+	      xPoints[xPointsPicked] = new Array();
+	      xPoints[xPointsPicked][0] = parseFloat(xi);
+	      xPoints[xPointsPicked][1] = parseFloat(yi);
+	      xPoints[xPointsPicked][2] = 1; // 1 if not filtered, 0 if processed already
+	      xPointsPicked = xPointsPicked + 1;
+	    }
     }
     
   }
@@ -772,6 +824,172 @@ function scanPlot()
   xPoints = [];	
   pointsStatus(pointsPicked);  
   return pointsPicked;
+}
+
+/**
+ * Auto extraction algorithm with predefined step sizes along X.
+ */
+function AE_xstep()
+{
+      closePopup("testImageWindow");
+
+      var xPointsPicked = 0;
+      xyData = [];
+      pointsPicked = 0;
+      
+      redrawCanvas();
+      markedScreen = currentScreen;
+      
+      var xStepEl = document.getElementById("xStepDX");
+      var xStep = parseFloat(xStepEl.value);
+      
+      var LineThicknessEl = document.getElementById("xStepDY");
+      var yStep = parseFloat(LineThicknessEl.value);
+      
+      var dw = canvasWidth;
+      var dh = canvasHeight;
+      
+      var blobAvg = new Array();
+      
+      var dx = 1;
+      var coli = 0;
+      
+      while (coli < dw)
+      {
+            blobs = -1;
+            firstbloby = -2.0*yStep;
+            bi = 0;
+    
+            for(var rowi = 0; rowi < dh; rowi++)
+            {
+                if (binaryData[rowi][coli] == true)
+                {
+                    dx = xStep; // First contact has been made, start moving forward with xStep now.
+                    
+                    if (rowi > firstbloby + yStep)
+                    {
+                        blobs = blobs + 1;
+	                    bi = 1;
+	                    blobAvg[blobs] = rowi;
+	                    firstbloby = rowi;
+	                }
+	                else
+	                {
+	                    bi = bi + 1;
+	                    blobAvg[blobs] = parseFloat((blobAvg[blobs]*(bi-1.0) + rowi)/parseFloat(bi));
+	                }
+                }
+                
+            }
+            
+            if (blobs >= 0)
+            {
+	            xi = coli;
+	            for (var blbi = 0; blbi <= blobs; blbi++)
+	            {
+	                  yi = blobAvg[blbi];
+                      xyData[pointsPicked] = new Array();
+                      xyData[pointsPicked][0] = parseFloat(xi);
+                      xyData[pointsPicked][1] = parseFloat(yi);
+                      pointsPicked = pointsPicked + 1;	
+                      
+                      ctx.beginPath();
+                      ctx.fillStyle = "rgb(200,0,200)";
+                      ctx.arc(parseInt(xi),parseInt(yi),3,0,2.0*Math.PI,true);
+                      ctx.fill();
+	            }
+            }
+
+            
+            coli = coli + dx;
+      }
+      
+      pointsStatus(pointsPicked);  
+      return pointsPicked;
+      
+}
+
+/**
+ * Auto extraction algorithm with predefined step sizes along Y.
+ */
+function AE_ystep()
+{
+      closePopup("testImageWindow");
+
+      var xPointsPicked = 0;
+      xyData = [];
+      pointsPicked = 0;
+      
+      redrawCanvas();
+      markedScreen = currentScreen;
+      
+      var yStepEl = document.getElementById("yStepDY");
+      var yStep = parseFloat(yStepEl.value);
+      
+      var LineThicknessEl = document.getElementById("yStepDX");
+      var xStep = parseFloat(LineThicknessEl.value);
+      
+      var dw = canvasWidth;
+      var dh = canvasHeight;
+      
+      var blobAvg = new Array();
+      
+      var dy = -1;
+      var rowi = dh-1;
+      
+      while (rowi >= 0)
+      {
+            blobs = -1;
+            firstblobx = -2.0*xStep;
+            bi = 0;
+    
+            for(var coli = 0; coli < dw; coli++)
+            {
+                if (binaryData[rowi][coli] == true)
+                {
+                    dy = -yStep; // First contact has been made, start moving forward with xStep now.
+                    
+                    if (coli > firstblobx + xStep)
+                    {
+                        blobs = blobs + 1;
+	                    bi = 1;
+	                    blobAvg[blobs] = coli;
+	                    firstblobx = coli;
+	                }
+	                else
+	                {
+	                    bi = bi + 1;
+	                    blobAvg[blobs] = parseFloat((blobAvg[blobs]*(bi-1.0) + coli)/parseFloat(bi));
+	                }
+                }
+                
+            }
+            
+            if (blobs >= 0)
+            {
+	            yi = rowi;
+	            for (var blbi = 0; blbi <= blobs; blbi++)
+	            {
+	                  xi = blobAvg[blbi];
+                      xyData[pointsPicked] = new Array();
+                      xyData[pointsPicked][0] = parseFloat(xi);
+                      xyData[pointsPicked][1] = parseFloat(yi);
+                      pointsPicked = pointsPicked + 1;	
+                      
+                      ctx.beginPath();
+                      ctx.fillStyle = "rgb(200,0,200)";
+                      ctx.arc(parseInt(xi),parseInt(yi),3,0,2.0*Math.PI,true);
+                      ctx.fill();
+	            }
+            }
+
+            
+            rowi = rowi + dy;
+      }
+      
+      pointsStatus(pointsPicked);  
+      return pointsPicked;
+      
 }
 
 /*
