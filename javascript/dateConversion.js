c0726995df68933a753115a28e93a9038648cee1
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2013-10-07 22:56:50 -0500

    Partial implementation of date conversion object. Input parsing has been added, output formatting needs work

--- javascript/dateConversion.js
+++ javascript/dateConversion.js
@@ -39,19 +39,114 @@ var dateConverter = {
 
 	// Convert to Julian Date
 	toJD: function(dateString) {
-				return 0;
+				var dateParts = dateString.split("/"),
+					year,
+					month,
+					day,
+					tempDate,
+					rtnValue;
+
+				if(dateParts.length <= 0 || dateParts.length > 3) {
+					return null;
+				}
+
+				year = parseInt(dateParts[0], 10);
+
+				month = parseInt(dateParts[1] === undefined ? 0 : dateParts[1], 10);
+
+				day = parseInt(dateParts[2] === undefined ? 1 : dateParts[2], 10);
+
+				if(isNaN(year) || isNaN(month) || isNaN(day)) {
+					return null;
+				}
+
+				if(month > 12 || month < 1) {
+					return null;
+				}
+
+				if(day > 31 || day < 1) {
+					return null;
+				}
+
+				// Temporary till I figure out julian dates:
+				tempDate = new Date();
+				tempDate.setUTCFullYear(year);
+				tempDate.setUTCMonth(month-1);
+				tempDate.setUTCDate(day);
+				rtnValue = parseFloat(Date.parse(tempDate));
+				if(!isNaN(rtnValue)) {
+					return rtnValue;
+				}
+				return null;
 			},
 
 	// Convert back from Julian Date
 	fromJD: function(jd) {
+
+				// Temporary till I figure out julian dates:
+				var msInDay = 24*60*60*1000,
+					roundedDate = Math.round(jd/msInDay)*msInDay,
+					tempDate = new Date(Date.parse(jd));
+
 				return {
-					year: 0,
-					month: 0,
-					day: 0
+					year: tempDate.getUTCFullYear(),
+					month: tempDate.getUTCMonth(),
+					day: tempDate.getUTCDate()
 				};
 			},
 
 	formatDate: function(dateObject, formatString) {
+				var longMonths = [
+									"January", 
+									"February", 
+									"March", 
+									"April", 
+									"May", 
+									"June", 
+									"July", 
+									"August", 
+									"September",
+									"October",
+									"November",
+									"December"
+								],
+					shortMonths = [
+									"Jan",
+									"Feb",
+									"Mar",
+									"Apr",
+									"May",
+									"Jun",
+									"Jul",
+									"Aug",
+									"Sep",
+									"Oct",
+									"Nov",
+									"Dec"
+								];
+
 				return dateObject.year + "/" + dateObject.month + "/" + dateObject.date;
+			},
+
+	getFormatString: function(dateString) {
+				var dateParts = dateString.split("/"),
+					year,
+					month,
+					day,
+					formatString = 'yyyy/mm/dd';
+				
+				if(dateParts.length >= 1) {
+					formatString = 'yyyy';
+				}
+
+				if(dateParts.length >= 2) {
+					formatString += '/mm';
+				}
+
+				if(dateParts.length === 3) {
+					formatString += '/dd';
+				}
+
+				return formatString;
 			}
 };
