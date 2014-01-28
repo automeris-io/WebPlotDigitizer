0ed9b5850aa2df55d0cc2e4df6cbebc1d63c441e
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-01-27 23:17:51 -0600

    hide globals in csv export related code

--- javascript/csvgenerator.js
+++ javascript/csvgenerator.js
@@ -21,221 +21,229 @@
 
 */
 
-/* Global variables to store generated data. Global stuff will be removed only in 3.0 :( */
 
-var rawCSVData,
-	displayData;
+var CSVExport = (function () {
 
-/*
- * Generate CSV.
- */
- function generateCSV() {
+    var rawCSVData,
+        displayData;
 
-    if((axesPicked === 1) && (pointsPicked >= 1)) {
-        popup.show('csvWindow');
-			
-		rawCSVData = pixelToData(xyData, pointsPicked, plotType);
+    // Generate CSV.
+    function generateCSV() {
 
-		var dataSortOrder = document.getElementById('dataSortOrder'),
-			dataSortOption = document.getElementById('dataSortOption'),
-			variableNames = document.getElementById('dataVariables');
+        if((axesPicked === 1) && (pointsPicked >= 1)) {
+            popup.show('csvWindow');
+                
+            rawCSVData = pixelToData(xyData, pointsPicked, plotType);
 
-		dataSortOption.innerHTML = '<option value="raw">Raw Output</option>';
+            var dataSortOrder = document.getElementById('dataSortOrder'),
+                dataSortOption = document.getElementById('dataSortOption'),
+                variableNames = document.getElementById('dataVariables');
 
-		if( (plotType === 'XY') || (plotType === 'map') || (plotType === 'image')) {
+            dataSortOption.innerHTML = '<option value="raw">Raw Output</option>';
 
-			dataSortOption.innerHTML += '<option value="0">x</option>';
-			dataSortOption.innerHTML += '<option value="1">y</option>';
-			variableNames.innerHTML = 'x, y';
+            if( (plotType === 'XY') || (plotType === 'map') || (plotType === 'image')) {
 
-		} else if ( (plotType === 'ternary') ) {
+                dataSortOption.innerHTML += '<option value="0">x</option>';
+                dataSortOption.innerHTML += '<option value="1">y</option>';
+                variableNames.innerHTML = 'x, y';
 
-			dataSortOption.innerHTML += '<option value="0">a</option>';
-			dataSortOption.innerHTML += '<option value="1">b</option>';
-			dataSortOption.innerHTML += '<option value="2">c</option>';
-			variableNames.innerHTML = 'a, b, c';
+            } else if ( (plotType === 'ternary') ) {
 
-		} else if ( (plotType === 'polar') ) {
+                dataSortOption.innerHTML += '<option value="0">a</option>';
+                dataSortOption.innerHTML += '<option value="1">b</option>';
+                dataSortOption.innerHTML += '<option value="2">c</option>';
+                variableNames.innerHTML = 'a, b, c';
 
-			dataSortOption.innerHTML += '<option value="0">r</option>';
-			dataSortOption.innerHTML += '<option value="1">Θ</option>';
-			variableNames.innerHTML = 'r, Θ';
-		}
+            } else if ( (plotType === 'polar') ) {
 
-		dataSortOption.innerHTML += '<option value="NearestNeighbor">Nearest Neighbor</option>';
+                dataSortOption.innerHTML += '<option value="0">r</option>';
+                dataSortOption.innerHTML += '<option value="1">Θ</option>';
+                variableNames.innerHTML = 'r, Θ';
+            }
 
-		updateCSVSortingControls();
+            dataSortOption.innerHTML += '<option value="NearestNeighbor">Nearest Neighbor</option>';
 
-		var dateFormattingEl = document.getElementById('csvDateFormatting');
-		if(plotType === 'XY') {
-			if((axesAlignmentData[6] === true || axesAlignmentData[7] === true)) {
-				dateFormattingEl.style.display = 'inline-block';
+            updateCSVSortingControls();
 
-				var xDateFormattingEl = document.getElementById('csvDateFormattingX');
-				var yDateFormattingEl = document.getElementById('csvDateFormattingY');
-				var xDateFormatEl = document.getElementById('xDateFormat');
-				var yDateFormatEl = document.getElementById('yDateFormat');
+            var dateFormattingEl = document.getElementById('csvDateFormatting');
+            if(plotType === 'XY') {
+                if((axesAlignmentData[6] === true || axesAlignmentData[7] === true)) {
+                    dateFormattingEl.style.display = 'inline-block';
 
-				if(axesAlignmentData[6]) {
-					xDateFormattingEl.style.display = 'inline-block';
-					xDateFormatEl.value = axesAlignmentData[8];
-				} else {	
-					xDateFormattingEl.style.display = 'none';
-				}
+                    var xDateFormattingEl = document.getElementById('csvDateFormattingX');
+                    var yDateFormattingEl = document.getElementById('csvDateFormattingY');
+                    var xDateFormatEl = document.getElementById('xDateFormat');
+                    var yDateFormatEl = document.getElementById('yDateFormat');
 
-				if(axesAlignmentData[7]) {
-					yDateFormattingEl.style.display = 'inline-block';
-					yDateFormat.value = axesAlignmentData[9];
-				} else {	
-					yDateFormattingEl.style.display = 'none';
-				}
-			} else {
-				dateFormattingEl.style.display = 'none';
-			}
-		} else {
-			dateFormattingEl.style.display = 'none';
-		}
-		
-		generateCSVTextFromData(rawCSVData);
+                    if(axesAlignmentData[6]) {
+                        xDateFormattingEl.style.display = 'inline-block';
+                        xDateFormatEl.value = axesAlignmentData[8];
+                    } else {	
+                        xDateFormattingEl.style.display = 'none';
+                    }
+
+                    if(axesAlignmentData[7]) {
+                        yDateFormattingEl.style.display = 'inline-block';
+                        yDateFormat.value = axesAlignmentData[9];
+                    } else {	
+                        yDateFormattingEl.style.display = 'none';
+                    }
+                } else {
+                    dateFormattingEl.style.display = 'none';
+                }
+            } else {
+                dateFormattingEl.style.display = 'none';
+            }
+            
+            generateCSVTextFromData(rawCSVData);
+        }
+     }
+
+     /**
+      * Select all data in text area.
+      */
+     function selectAllCSVData() {
+        var tarea = document.getElementById('tarea');
+        tarea.focus();
+        tarea.select();
+     }
+
+    /**
+     * Update CSV sorting controls.
+     */
+    function updateCSVSortingControls() {
+        var dataSortOption = document.getElementById('dataSortOption'),
+            dataSortOrder = document.getElementById('dataSortOrder');
+        
+        if(dataSortOption.value === 'NearestNeighbor' || dataSortOption.value === 'raw') {
+            dataSortOrder.setAttribute('disabled', true);	
+        } else {
+            dataSortOrder.removeAttribute('disabled');
+        }
+
+        reSortCSV();
     }
- }
 
- /**
-  * Select all data in text area.
-  */
- function selectAllCSVData() {
- 	var tarea = document.getElementById('tarea');
-	tarea.focus();
-	tarea.select();
- }
+    /**
+     * Dump data to the CSV text area
+     */
+    function generateCSVTextFromData(retData) {
 
-/**
- * Update CSV sorting controls.
- */
-function updateCSVSortingControls() {
-	var dataSortOption = document.getElementById('dataSortOption'),
-		dataSortOrder = document.getElementById('dataSortOrder');
-	
-	if(dataSortOption.value === 'NearestNeighbor' || dataSortOption.value === 'raw') {
-		dataSortOrder.setAttribute('disabled', true);	
-	} else {
-		dataSortOrder.removeAttribute('disabled');
-	}
+        var tarea = document.getElementById('tarea');
+            tarea.value = '';
 
-	reSortCSV();
-}
+        if((plotType === 'XY') || (plotType === 'map') || (plotType === 'polar') || (plotType === 'image')) {
+            for(var ii = 0; ii < pointsPicked; ii++) {
+                tarea.value = tarea.value + formatVariableInCSV(retData[ii][0], 'X') + ',' + formatVariableInCSV(retData[ii][1], 'Y') + '\n';
+            }
+        } else if((plotType === 'ternary')) {
+            for(var ii = 0; ii < pointsPicked; ii++) {
+                tarea.value = tarea.value + retData[ii][0] + ',' + retData[ii][1] + ',' + retData[ii][2] + '\n';
+            }
+        }
 
-/**
- * Dump data to the CSV text area
- */
-function generateCSVTextFromData(retData) {
+        displayData = retData;
+    }
 
-	var tarea = document.getElementById('tarea');
-		tarea.value = '';
+    /**
+     * Format variable 
+     */
+    function formatVariableInCSV(val, variableType) {
+        if(plotType === 'XY') {
+            if(variableType === 'X' && axesAlignmentData[6]) {
+                var xDateFormatEl = document.getElementById('xDateFormat');
+                return dateConverter.formatDate(dateConverter.fromJD(val), xDateFormatEl.value);			
+            }
+            if(variableType === 'Y' && axesAlignmentData[7]) {
+                var yDateFormatEl = document.getElementById('yDateFormat');
+                return dateConverter.formatDate(dateConverter.fromJD(val), yDateFormatEl.value);
+            }
+        }
+        return val;
+    }
 
-	if((plotType === 'XY') || (plotType === 'map') || (plotType === 'polar') || (plotType === 'image')) {
-		for(var ii = 0; ii < pointsPicked; ii++) {
-			tarea.value = tarea.value + formatVariableInCSV(retData[ii][0], 'X') + ',' + formatVariableInCSV(retData[ii][1], 'Y') + '\n';
-		}
-	} else if((plotType === 'ternary')) {
-		for(var ii = 0; ii < pointsPicked; ii++) {
-			tarea.value = tarea.value + retData[ii][0] + ',' + retData[ii][1] + ',' + retData[ii][2] + '\n';
-		}
-	}
+    /**
+     * Resort data
+     */
+    function reSortCSV() {
+        var dataSortOption = document.getElementById('dataSortOption'),
+            dataSortOrder = document.getElementById('dataSortOrder'),
+            
+            isAscending = dataSortOrder.value === 'ascending',
+            isRaw = dataSortOption.value == 'raw',
+            isConnectivity = dataSortOption.value === 'NearestNeighbor',
+            dataIndex,
+            sortedData = rawCSVData.slice(0),
+            plotDim = (plotType === 'ternary') ? 3 : 2;
 
-	displayData = retData;
-}
+        if(isRaw) {
+            generateCSVTextFromData(sortedData);
+            return;
+        }
 
-/**
- * Format variable 
- */
-function formatVariableInCSV(val, variableType) {
-	if(plotType === 'XY') {
-		if(variableType === 'X' && axesAlignmentData[6]) {
-			var xDateFormatEl = document.getElementById('xDateFormat');
-			return dateConverter.formatDate(dateConverter.fromJD(val), xDateFormatEl.value);			
-		}
-		if(variableType === 'Y' && axesAlignmentData[7]) {
-			var yDateFormatEl = document.getElementById('yDateFormat');
-			return dateConverter.formatDate(dateConverter.fromJD(val), yDateFormatEl.value);
-		}
-	}
-	return val;
-}
+        if(!isConnectivity) {
+            dataIndex = parseInt(dataSortOption.value, 10);
+            if((dataIndex < 0) || (dataIndex >= 3)) return;
 
-/**
- * Resort data
- */
-function reSortCSV() {
-	var dataSortOption = document.getElementById('dataSortOption'),
-		dataSortOrder = document.getElementById('dataSortOrder'),
-		
-		isAscending = dataSortOrder.value === 'ascending',
-		isRaw = dataSortOption.value == 'raw',
-		isConnectivity = dataSortOption.value === 'NearestNeighbor',
-		dataIndex,
-		sortedData = rawCSVData.slice(0),
-		plotDim = (plotType === 'ternary') ? 3 : 2;
+            sortedData.sort(function(a,b) {
+                if(a[dataIndex] > b[dataIndex]) {
+                    return isAscending ? 1 : -1;
+                } else if (a[dataIndex] < b[dataIndex]) {
+                    return isAscending ? -1 : 1;
+                }
+                return 0;			
+            });
 
-	if(isRaw) {
-		generateCSVTextFromData(sortedData);
-		return;
-	}
+            generateCSVTextFromData(sortedData);
+            return;
+        }
 
-	if(!isConnectivity) {
-		dataIndex = parseInt(dataSortOption.value, 10);
-		if((dataIndex < 0) || (dataIndex >= 3)) return;
+        if(isConnectivity) {
+            var mindist, compdist, minindex,
+                swapVariable = [1.0, 1.0, 1.0];
 
-		sortedData.sort(function(a,b) {
-			if(a[dataIndex] > b[dataIndex]) {
-				return isAscending ? 1 : -1;
-			} else if (a[dataIndex] < b[dataIndex]) {
-				return isAscending ? -1 : 1;
-			}
-			return 0;			
-		});
+            for(var ii = 0; ii < pointsPicked-1; ii++) {
+                minindex = -1;
 
-		generateCSVTextFromData(sortedData);
-		return;
-	}
+                for(var jj = ii + 1; jj < pointsPicked; jj++) {
+                    compdist = (sortedData[ii][0] - sortedData[jj][0])*(sortedData[ii][0] - sortedData[jj][0]) + 
+                                (sortedData[ii][1] - sortedData[jj][1])*(sortedData[ii][1] - sortedData[jj][1]);
+                    if(plotDim === 3) {
+                        compdist += (sortedData[ii][2] - sortedData[jj][2])*(sortedData[ii][2] - sortedData[jj][2]);
+                    }
+                    if((compdist < mindist) || (minindex === -1)) {
+                        mindist = compdist;
+                        minindex = jj;
+                    }
+                }
 
-	if(isConnectivity) {
-		var mindist, compdist, minindex,
-			swapVariable = [1.0, 1.0, 1.0];
+                swapVariable[0] = sortedData[minindex][0];
+                sortedData[minindex][0] = sortedData[ii+1][0];
+                sortedData[ii+1][0] = swapVariable[0];
 
-		for(var ii = 0; ii < pointsPicked-1; ii++) {
-			minindex = -1;
+                swapVariable[1] = sortedData[minindex][1];
+                sortedData[minindex][1] = sortedData[ii+1][1];
+                sortedData[ii+1][1] = swapVariable[1];
 
-			for(var jj = ii + 1; jj < pointsPicked; jj++) {
-				compdist = (sortedData[ii][0] - sortedData[jj][0])*(sortedData[ii][0] - sortedData[jj][0]) + 
-							(sortedData[ii][1] - sortedData[jj][1])*(sortedData[ii][1] - sortedData[jj][1]);
-				if(plotDim === 3) {
-					compdist += (sortedData[ii][2] - sortedData[jj][2])*(sortedData[ii][2] - sortedData[jj][2]);
-				}
-				if((compdist < mindist) || (minindex === -1)) {
-					mindist = compdist;
-					minindex = jj;
-				}
-			}
+                if(plotDim === 3) {
+                    swapVariable[2] = sortedData[minindex][2];
+                    sortedData[minindex][2] = sortedData[ii+1][2];
+                    sortedData[ii+1][2] = swapVariable[2];
+                }
+            }
 
-			swapVariable[0] = sortedData[minindex][0];
-			sortedData[minindex][0] = sortedData[ii+1][0];
-			sortedData[ii+1][0] = swapVariable[0];
+            generateCSVTextFromData(sortedData);
+            return;
+        }
 
-			swapVariable[1] = sortedData[minindex][1];
-			sortedData[minindex][1] = sortedData[ii+1][1];
-			sortedData[ii+1][1] = swapVariable[1];
+    }
 
-			if(plotDim === 3) {
-				swapVariable[2] = sortedData[minindex][2];
-				sortedData[minindex][2] = sortedData[ii+1][2];
-				sortedData[ii+1][2] = swapVariable[2];
-			}
-		}
+    return {
+        generate: generateCSV,
+        reSort: reSortCSV,
+        updateSortingControls: updateCSVSortingControls,
+        selectAll: selectAllCSVData
+    };
 
-		generateCSVTextFromData(sortedData);
-		return;
-	}
-
-}
+})();
 
