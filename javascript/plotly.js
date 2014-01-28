f7bbe55a45c0736199f1c59c25b6f4e0efdebcf7
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-01-27 22:50:28 -0600

    hiding globals for plotly related code

--- javascript/plotly.js
+++ javascript/plotly.js
@@ -21,71 +21,77 @@
 
 */
 
-/* This file contains code to export CSV data to an external software called Plotly (http://plot.ly) */
+// This module contains methods to export CSV data to an external software called Plotly (http://plot.ly)
+var plotly = (function() {
+
+    // Dump the contents of the global variable displayData into Plotly
+    function exportData() {
+
+        if(pointsPicked === 0) return;
+
+        var formContainer = document.createElement('div'),
+            formElement = document.createElement('form'),
+            formData = document.createElement('input');
+        
+        
+        formElement.setAttribute('method', 'post');
+        formElement.setAttribute('action', 'https://plot.ly/external');
+        formElement.setAttribute('target', '_blank');
+        
+        formData.setAttribute('type', "text");
+        formData.setAttribute('name', "data");
+
+        formElement.appendChild(formData);
+        formContainer.appendChild(formElement);
+        document.body.appendChild(formContainer);
+        formContainer.style.display = 'none';
 
 
-/* Dump the contents of the global variable displayData into Plotly
-*/
-function exportToPlotly() {
+        var xDisplayData = [],
+            yDisplayData = [],
+            zDisplayData = [],
+            jsonData = { data: [] };
+        
+        if((plotType === 'XY') || (plotType === 'map') || (plotType === 'polar') || (plotType === 'image')) {
+            for(var ii = 0; ii < pointsPicked; ii++) {
+                xDisplayData[ii] = formatVariable(displayData[ii][0], 'X');
+                yDisplayData[ii] = formatVariable(displayData[ii][1], 'Y');
+            }
+            jsonData.data[0] = {x: xDisplayData, y: yDisplayData};
 
-	if(pointsPicked === 0) return;
+        } else if((plotType === 'ternary')) {
+            for(var ii = 0; ii < pointsPicked; ii++) {
+                xDisplayData[ii] = displayData[ii][0];
+                yDisplayData[ii] = displayData[ii][1];
+                zDisplayData[ii] = displayData[ii][2];
+            }
+            jsonData.data[0] = {x: xDisplayData, y: yDisplayData, z: zDisplayData};
+        }
 
-	var formContainer = document.createElement('div'),
-		formElement = document.createElement('form'),
-		formData = document.createElement('input');
-	
-	
-	formElement.setAttribute('method', 'post');
-	formElement.setAttribute('action', 'https://plot.ly/external');
-	formElement.setAttribute('target', '_blank');
-	
-	formData.setAttribute('type', "text");
-	formData.setAttribute('name', "data");
+        formData.setAttribute('value', JSON.stringify(jsonData));
 
-	formElement.appendChild(formData);
-	formContainer.appendChild(formElement);
-	document.body.appendChild(formContainer);
-	formContainer.style.display = 'none';
+        formElement.submit();
 
+        document.body.removeChild(formContainer);
+    }
+    
+    // format variable as per Plotly's requirements
+    function formatVariable(val, variableType) {
+        var formatString = 'yyyy-mm-dd';
 
-	var xDisplayData = [],
-		yDisplayData = [],
-		zDisplayData = [],
-		jsonData = { data: [] };
-	
-	if((plotType === 'XY') || (plotType === 'map') || (plotType === 'polar') || (plotType === 'image')) {
-		for(var ii = 0; ii < pointsPicked; ii++) {
-			xDisplayData[ii] = formatVariableForPlotly(displayData[ii][0], 'X');
-			yDisplayData[ii] = formatVariableForPlotly(displayData[ii][1], 'Y');
-		}
-		jsonData.data[0] = {x: xDisplayData, y: yDisplayData};
+        if(plotType === 'XY') {
+            if(variableType === 'X' && axesAlignmentData[6]) {
+                return dateConverter.formatDate(dateConverter.fromJD(val), formatString);			
+            }
+            if(variableType === 'Y' && axesAlignmentData[7]) {
+                return dateConverter.formatDate(dateConverter.fromJD(val), formatString);
+            }
+        }
+        return val;
+    }
 
-	} else if((plotType === 'ternary')) {
-		for(var ii = 0; ii < pointsPicked; ii++) {
-			xDisplayData[ii] = displayData[ii][0];
-			yDisplayData[ii] = displayData[ii][1];
-			zDisplayData[ii] = displayData[ii][2];
-		}
-		jsonData.data[0] = {x: xDisplayData, y: yDisplayData, z: zDisplayData};
-	}
+    return {
+        exportData: exportData
+    };
+})();
 
-	formData.setAttribute('value', JSON.stringify(jsonData));
-
-	formElement.submit();
-
-	document.body.removeChild(formContainer);
-}
-
-function formatVariableForPlotly(val, variableType) {
-	var formatString = 'yyyy-mm-dd';
-
-	if(plotType === 'XY') {
-		if(variableType === 'X' && axesAlignmentData[6]) {
-			return dateConverter.formatDate(dateConverter.fromJD(val), formatString);			
-		}
-		if(variableType === 'Y' && axesAlignmentData[7]) {
-			return dateConverter.formatDate(dateConverter.fromJD(val), formatString);
-		}
-	}
-	return val;
-}
