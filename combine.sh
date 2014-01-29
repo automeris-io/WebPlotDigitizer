d5b31ea2daa606a58bcda417304572146b425bd9
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-01-28 19:21:13 -0600

    reorganizing javascript files into categories

--- combine.sh
+++ combine.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 cat javascript/*.js > combined.js
-cat javascript/AEalgos/*.js >> combined.js
+cat javascript/core/*.js >> combined.js
+cat javascript/core/AEalgos/*.js >> combined.js
+cat javascript/widgets/*.js >> combined.js
+cat javascript/tools/*.js >> combined.js
+cat javascript/dataExport/*.js >> combined.js
+cat javascript/browser/*.js >> combined.js
+cat javascript/core/AEalgos/*.js >> combined.js
+
