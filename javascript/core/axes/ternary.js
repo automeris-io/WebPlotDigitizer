c906b2e4565e9c4530cc87d6ecd64fa9daa8a2b1
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-02-20 23:27:37 -0600

    added transformation algorithm for ternary and polar axes

--- javascript/core/axes/ternary.js
+++ javascript/core/axes/ternary.js
@@ -26,9 +26,33 @@ var wpd = wpd || {};
 wpd.TernaryAxes = (function () {
     var AxesObj = function () {
         var isCalibrated = false,
-            range100 = false,
-            isNormal = false,
+            
+            x0, y0, x1, y1, x2, y2, L,
+            phi0, root3, isRange0to100,
+            isOrientationNormal,
+
             processCalibration = function(cal, range100, is_normal) {  
+                var cp0 = cal.getPoint(0),
+                    cp1 = cal.getPoint(1),
+                    cp2 = cal.getPoint(2);
+
+                x0 = cp0.px;
+                y0 = cp0.py;
+                x1 = cp1.px;
+                y1 = cp1.py;
+                x2 = cp2.px;
+                y2 = cp2.py;
+
+                L = Math.sqrt((x0-x1)*(x0-x1) + (y0-y1)*(y0-y1));
+
+                phi0 = wpd.taninverse(-(y1-y0),x1-x0);
+
+                root3 = Math.sqrt(3);
+
+                isRange0to100 = range100;
+
+                isOrientationNormal = is_normal;
+
                 return true;
             };
 
@@ -42,10 +66,43 @@ wpd.TernaryAxes = (function () {
         };
 
         this.pixelToData = function(pxi, pyi) {
-            var data = [];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
+            var data = [],
+                rp,
+                thetap,
+                xx,
+                yy,
+                ap, bp, cp, bpt;
+
+            xp = parseFloat(pxi);
+            yp = parseFloat(pyi);
+
+            rp = Math.sqrt((xp-x0)*(xp-x0)+(yp-y0)*(yp-y0));
+
+            thetap = wpd.taninverse(-(yp-y0),xp-x0) - phi0;
+
+            xx = (rp*Math.cos(thetap))/L;
+		    yy = (rp*Math.sin(thetap))/L;
+			
+			ap = 1.0 - xx - yy/root3;
+			bp = xx - yy/root3;
+			cp = 2.0*yy/root3;
+			
+			if(isOrientationNormal == false) {
+                // reverse axes orientation
+			    bpt = bp;
+			    bp = ap;
+			    ap = cp;
+			    cp = bpt;
+			      				  
+			}
+			
+			if (isRange0to100 == true) {
+			    ap = ap*100; bp = bp*100; cp = cp*100;
+			}
+
+            data[0] = ap;
+            data[1] = bp;
+            data[2] = cp;
             return data;
         };
 
