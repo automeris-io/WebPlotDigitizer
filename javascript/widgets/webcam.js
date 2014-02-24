69487896ac120f5eb204762712fbb7c1d4070d81
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-02-24 00:16:11 -0600

    missed adding webcam javascript file

---
+++ javascript/widgets/webcam.js
@@ -0,0 +1,89 @@
+/*
+	WebPlotDigitizer - http://arohatgi.info/WebPlotDigitizer
+
+	Copyright 2010-2013 Ankit Rohatgi <ankitrohatgi@hotmail.com>
+
+	This file is part of WebPlotDigitizer.
+
+    WebPlotDigitizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebPlotDigitizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+var wpd = wpd || {};
+
+wpd.webcamCapture = (function () {
+
+    var cameraStream;
+
+    function isSupported() {
+        return !(getUserMedia() == null);
+    }
+
+    function unsupportedBrowser() {
+    }
+
+    function getUserMedia() {
+        return navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    }
+
+    function start() {
+        if(!isSupported()) {
+            unsupportedBrowser();
+            return;
+        }
+        wpd.popup.show('webcamCapture'); 
+        var $camVideo = document.getElementById('webcamVideo');
+        navigator.getUserMedia = getUserMedia();
+        navigator.getUserMedia({video: true}, function(stream) {
+            cameraStream = stream;
+            $camVideo.src = window.URL.createObjectURL(stream);
+  		}, function() {}); 
+    }
+
+    function capture() {
+        var $webcamCanvas = document.createElement('canvas'),
+            $camVideo = document.getElementById('webcamVideo'),
+            webcamCtx = $webcamCanvas.getContext('2d'),
+            imageData;
+        $webcamCanvas.width = $camVideo.videoWidth;
+        $webcamCanvas.height = $camVideo.videoHeight;
+        webcamCtx.drawImage($camVideo, 0, 0);
+        imageData = webcamCtx.getImageData(0, 0, $webcamCanvas.width, $webcamCanvas.height);
+        cameraOff();
+        wpd.graphicsWidget.runImageOp(function() {
+            return {
+                imageData: imageData,
+                width: $webcamCanvas.width,
+                height: $webcamCanvas.height
+            };
+        });
+    }
+
+    function cameraOff() {
+        cameraStream.stop();
+        wpd.popup.close('webcamCapture'); 
+    }
+
+    function cancel() {
+        cameraOff();
+    }
+
+    return {
+        start: start,
+        cancel: cancel,
+        capture: capture
+    };
+
+})();
