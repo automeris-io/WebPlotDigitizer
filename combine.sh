234e7506c43ab70d055e3828efc5f9fdbcf05b6c
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-03-04 22:50:37 -0600

    updating combined and compiled script files and also index.html

--- combine.sh
+++ combine.sh
@@ -7,6 +7,6 @@ cat javascript/core/axes/*.js >> combined.js
 cat javascript/widgets/*.js >> combined.js
 cat javascript/tools/*.js >> combined.js
 cat javascript/dataExport/*.js >> combined.js
+cat javascript/services/*.js >> combined.js
 cat javascript/browser/*.js >> combined.js
-cat javascript/core/AEalgos/*.js >> combined.js
 
