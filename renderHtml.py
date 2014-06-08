976d87d0538571cac21f1baff9aea7286c59bea7
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-06-07 21:53:04 -0500

    Splitting html pages down into jinja templates. The HTML views can now be modularized much more easily. Also added a build script to compress and render pages.

---
+++ renderHtml.py
@@ -0,0 +1,19 @@
+# Render HTML pages for WebPlotDigitizer
+
+from jinja2 import Environment, FileSystemLoader, Template
+
+env = Environment(loader=FileSystemLoader('templates'))
+
+def renderPage(filename):
+    print "Rendering", filename
+    pageTemplate = env.get_template(filename)
+    page = pageTemplate.render()
+    pageFile = open(filename, 'w')
+    pageFile.write(page.encode("utf8"))
+    pageFile.close()
+
+
+renderPage("dev.html")
+renderPage("index.html")
+renderPage("offline.html")
+    
