acaa3db8157494f32b6f93df10c1cd16fcdbf267
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-06-22 03:09:08 -0500

    Starting to add grid detection module.

--- combined.js
+++ combined.js
@@ -3710,6 +3710,32 @@ wpd.messagePopup = (function () {
     };
 })();
 
+wpd.okCancelPopup = (function () {
+    var okCallback, cancelCallback;
+
+    function show(title, msg, ok_callback, cancel_callback) {
+        wpd.popup.show('okCancelPopup');
+        document.getElementById('ok-cancel-popup-heading').innerHTML = title;
+        document.getElementById('ok-cancel-popup-text').innerHTML = msg;
+    }
+
+    function ok() {
+        wpd.popup.close('okCancelPopup');
+        okCallback();
+    }
+
+    function cancel() {
+        wpd.popup.close('okCancelPopup');
+        cancelCallback();
+    }
+
+    return {
+        show: show,
+        ok: ok,
+        cancel: cancel
+    };
+})();
+
 wpd.unsupported = function () {
     wpd.messagePopup.show("Unsupported Feature!", "This feature has not been implemented in the current version. This may be available in a future release.");
 };
@@ -4830,6 +4856,41 @@ wpd.ColorFilterRepainter = (function () {
         };
     }
     return Painter;
+})();
+/*
+	WebPlotDigitizer - http://arohatgi.info/WebPlotDigitizer
+
+	Copyright 2010-2014 Ankit Rohatgi <ankitrohatgi@hotmail.com>
+
+	This file is part of WebPlotDigitizer.
+
+    WebPlotDigitizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebPlotDigitizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+wpd = wpd || {};
+
+wpd.gridDetection = (function () {
+    
+    function start() {
+        wpd.sidebar.show('grid-detection-sidebar');
+    }
+
+    return {
+        start: start
+    };
 })();
 /*
 	WebPlotDigitizer - http://arohatgi.info/WebPlotDigitizer
