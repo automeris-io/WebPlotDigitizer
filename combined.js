24cfa687f2faba6d87bd53f68bcfded0409927ca
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-06-23 23:30:31 -0500

    Add grid mask drawing tools to help mark the region for grid removal.

--- combined.js
+++ combined.js
@@ -4969,12 +4969,195 @@ wpd = wpd || {};
 wpd.gridDetection = (function () {
     
     function start() {
+        wpd.graphicsWidget.removeTool();
+        wpd.graphicsWidget.removeRepainter();
+        wpd.graphicsWidget.resetData();
         wpd.sidebar.show('grid-detection-sidebar');
     }
 
+    function markBox() {
+        var tool = new wpd.GridBoxTool();
+        wpd.graphicsWidget.setTool(tool);
+    }
+
+    function viewMask() {
+        var tool = new wpd.GridViewMaskTool();
+        wpd.graphicsWidget.setTool(tool);
+    }
+
+    function clearMask() {
+        wpd.graphicsWidget.removeTool();
+        wpd.graphicsWidget.removeRepainter();
+        wpd.appData.getPlotData().getAutoDetector().gridMask = null;
+        wpd.graphicsWidget.resetData();
+    }
+
+    function grabMask() {
+        // Mask is just a list of pixels with the yellow color in the data layer
+        var ctx = wpd.graphicsWidget.getAllContexts(),
+            imageSize = wpd.graphicsWidget.getImageSize(),
+            maskDataPx = ctx.oriDataCtx.getImageData(0, 0, imageSize.width, imageSize.height),
+            maskData = [],
+            i,
+            mi = 0,
+            autoDetector = wpd.appData.getPlotData().getAutoDetector();
+        for(i = 0; i < maskDataPx.data.length; i+=4) {
+            if (maskDataPx.data[i] === 255 && maskDataPx.data[i+1] === 255 && maskDataPx.data[i+2] === 0) {
+                maskData[mi] = i/4; mi++;
+            }
+        }
+        autoDetector.gridMask = maskData;
+    }
+
     return {
-        start: start
+        start: start,
+        markBox: markBox,
+        clearMask: clearMask,
+        viewMask: viewMask,
+        grabMask: grabMask
     };
+})();
+
+// TODO: Think of reusing mask.js code here
+wpd.GridBoxTool = (function () {
+    var Tool = function () {
+        var isDrawing = false,
+            topImageCorner,
+            topScreenCorner,
+            ctx = wpd.graphicsWidget.getAllContexts(),
+            moveTimer,
+            screen_pos,
+
+            mouseMoveHandler = function () {
+                wpd.graphicsWidget.resetHover();
+                ctx.hoverCtx.strokeStyle = "rgb(0,0,0)";
+    		    ctx.hoverCtx.strokeRect(topScreenCorner.x, topScreenCorner.y, screen_pos.x - topScreenCorner.x, screen_pos.y - topScreenCorner.y);
+            },
+
+            mouseUpHandler = function (ev, pos, imagePos) {
+                if(isDrawing === false) {
+                    return;
+                }
+                clearTimeout(moveTimer);
+                isDrawing = false;
+                wpd.graphicsWidget.resetHover();
+                ctx.dataCtx.fillStyle = "rgba(255,255,0,0.8)";
+                ctx.dataCtx.fillRect(topScreenCorner.x, topScreenCorner.y, pos.x-topScreenCorner.x, pos.y-topScreenCorner.y);
+                ctx.oriDataCtx.fillStyle = "rgba(255,255,0,0.8)";
+                ctx.oriDataCtx.fillRect(topImageCorner.x, topImageCorner.y, imagePos.x - topImageCorner.x, imagePos.y - topImageCorner.y);
+            },
+
+            mouseOutPos = null,
+            mouseOutImagePos = null;
+
+        this.onAttach = function () {
+            wpd.graphicsWidget.setRepainter(new wpd.GridMaskPainter());
+            document.getElementById('grid-mask-box').classList.add('pressed-button');
+            document.getElementById('grid-mask-view').classList.add('pressed-button');
+        };
+
+        this.onMouseDown = function (ev, pos, imagePos) {
+            if(isDrawing === true) return;
+            isDrawing = true;
+            topImageCorner = imagePos;
+            topScreenCorner = pos;
+        };
+
+        this.onMouseMove = function (ev, pos, imagePos) {
+            if(isDrawing === false) return;
+            screen_pos = pos;
+            clearTimeout(moveTimer);
+            moveTimer = setTimeout(mouseMoveHandler, 2);
+        };
+
+        this.onMouseOut = function (ev, pos, imagePos) {
+            if(isDrawing === true) {
+                clearTimeout(moveTimer);
+                mouseOutPos = pos;
+                mouseOutImagePos = imagePos;
+            }
+        };
+
+        this.onDocumentMouseUp = function (ev, pos, imagePos) {
+            if (mouseOutPos != null && mouseOutImagePos != null) {
+                mouseUpHandler(ev, mouseOutPos, mouseOutImagePos);
+            } else {
+                mouseUpHandler(ev, pos, imagePos);
+            }
+            mouseOutPos = null;
+            mouseOutImagePos = null;
+        };
+
+        this.onMouseUp = function (ev, pos, imagePos) {
+            mouseUpHandler(ev, pos, imagePos);
+        };
+
+        this.onRemove = function () {
+            document.getElementById('grid-mask-box').classList.remove('pressed-button');
+            document.getElementById('grid-mask-view').classList.remove('pressed-button');
+            wpd.gridDetection.grabMask();
+        };
+    };
+    return Tool;
+})();
+
+
+wpd.GridViewMaskTool = (function () {
+    var Tool = function() {
+
+        this.onAttach = function () {
+            wpd.graphicsWidget.setRepainter(new wpd.GridMaskPainter());
+            document.getElementById('grid-mask-view').classList.add('pressed-button');
+        };
+
+        this.onRemove = function () {
+            document.getElementById('grid-mask-view').classList.remove('pressed-button');
+            wpd.gridDetection.grabMask();
+        };
+    };
+
+    return Tool;
+})();
+
+
+wpd.GridMaskPainter = (function () {
+    var Painter = function () {
+
+        var ctx = wpd.graphicsWidget.getAllContexts(),
+            autoDetector = wpd.appData.getPlotData().getAutoDetector(),
+            painter = function () {
+                if(autoDetector.gridMask == null || autoDetector.gridMask.length === 0) {
+                    return;
+                }
+                var maski, img_index,
+                    imageSize = wpd.graphicsWidget.getImageSize();
+                    imgData = ctx.oriDataCtx.getImageData(0, 0, imageSize.width, imageSize.height);
+
+                for(maski = 0; maski < autoDetector.gridMask.length; maski++) {
+                    img_index = autoDetector.gridMask[maski];
+                    imgData.data[img_index*4] = 255;
+                    imgData.data[img_index*4+1] = 255;
+                    imgData.data[img_index*4+2] = 0;
+                    imgData.data[img_index*4+3] = 200;
+                }
+
+                ctx.oriDataCtx.putImageData(imgData, 0, 0);
+                wpd.graphicsWidget.copyImageDataLayerToScreen();
+            };
+
+        this.painterName = 'gridMaskPainter';
+
+        this.onRedraw = function () {
+            wpd.gridDetection.grabMask();
+            painter();
+        };
+
+        this.onAttach = function () {
+            wpd.graphicsWidget.resetData();
+            painter();
+        };
+    };
+    return Painter;
 })();
 /*
 	WebPlotDigitizer - http://arohatgi.info/WebPlotDigitizer
@@ -5487,32 +5670,6 @@ wpd.dataMask = (function () {
         autoDetector.mask = maskData;
     }
 
-    function drawMask() {
-        var maskData = wpd.appData.getPlotData().getAutoDetector().mask;
-        
-        if(maskData == null || maskData.length === 0) {
-            return;
-        }
-
-        var i, ix, iy, img_index, scr_index,
-            ctx = wpd.graphicsWidget.getAllContexts(),
-            imageSize = wpd.graphicsWidget.getImageSize(),
-            oriData = ctx.oriDataCtx.getImageData(0, 0, imageSize.width, imageSize.height);
-
-        for(i = 0; i < maskData.length; i++) {
-            img_index = maskData[i];
-            ix = img_index % imageSize.width;
-            iy = parseInt(img_index / imageSize.height, 10);
-            oriData.data[img_index*4] = 255;
-            oriData.data[img_index*4+1] = 255;
-            oriData.data[img_index*4+2] = 0;
-            oriData.data[img_index*4+3] = 200;
-        }
-
-        ctx.oriDataCtx.putImageData(oriData, 0, 0, imageSize.width, imageSize.height);
-        wpd.graphicsWidget.copyImageDataLayerToScreen();
-    }
-
     function markBox() {
         var tool = new wpd.BoxMaskTool();
         wpd.graphicsWidget.setTool(tool);
@@ -5535,7 +5692,6 @@ wpd.dataMask = (function () {
 
     return {
         grabMask: grabMask,
-        drawMask: drawMask,
         markBox: markBox,
         markPen: markPen,
         eraseMarks: eraseMarks,
