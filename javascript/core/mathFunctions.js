5d14e0804c7084b9aafb69e60b1b86606752ceac
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2014-09-08 21:50:21 -0500

    Simplified XY Axes mapping code using a newer and extremely simple derivation. This should make constructing the transformation equations very easy.

--- javascript/core/mathFunctions.js
+++ javascript/core/mathFunctions.js
@@ -54,3 +54,40 @@ wpd.dist2d = function (x1, y1, x2, y2) {
 wpd.dist3d = function (x1, y1, z1, x2, y2, z2) {
     return Math.sqrt(wpd.sqDist3d(x1, y1, z1, x2, y2, z2));
 };
+
+wpd.mat = (function () {
+    
+    function det2x2(m) {
+        return m[0]*m[3] - m[1]*m[2];
+    }
+
+    function inv2x2(m) {
+        var det = det2x2(m);
+        return [m[3]/det, -m[1]/det, -m[2]/det, m[0]/det];
+    }
+
+    function mult2x2(m1, m2) {
+        return [
+                    m1[0]*m2[0] + m1[1]*m2[2], 
+                    m1[0]*m2[1] + m1[1]*m2[3], 
+                    m1[2]*m2[0] + m1[3]*m2[2], 
+                    m1[2]*m2[1] + m1[3]*m2[3]
+               ];
+    }
+
+    function mult2x2Vec(m, v) {
+        return [m[0]*v[0] + m[1]*v[1], m[2]*v[0] + m[3]*v[1]];
+    }
+
+    function multVec2x2(v, m) {
+        return [m[0]*v[0] + m[2]*v[1], m[1]*v[0] + m[3]*v[1]];
+    }
+
+    return {
+        det2x2: det2x2,
+        inv2x2: inv2x2,
+        mult2x2: mult2x2,
+        mult2x2Vec: mult2x2Vec,
+        multVec2x2: multVec2x2
+    };
+})();
