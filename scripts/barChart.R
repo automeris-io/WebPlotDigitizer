bf830a1460432d8965e05e7a733731bffd84d045
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-01-20 00:11:34 -0600

    Starting to add support for bar charts.

---
+++ scripts/barChart.R
@@ -0,0 +1,10 @@
+# A simple bar chart
+
+rm(list=ls())
+
+counts <- table(mtcars$carb)
+
+png(filename="barchart.png", width=300, height=200, units="px", pointsize=14, res=NA)
+par(mar=c(2,2,1,1))
+barplot(counts, col=c("darkgreen"))
+dev.off()
