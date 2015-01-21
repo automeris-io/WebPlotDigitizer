7680aeb6ecd0aadc2ac8135b5f9a0d1c9106f842
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-01-20 21:28:04 -0600

    Update bar chart image

--- scripts/barChart.R
+++ scripts/barChart.R
@@ -4,8 +4,13 @@ rm(list=ls())
 
 counts <- table(mtcars$carb)
 
-png(filename="barchart.svg", width=300, height=200, units="px", pointsize=14)
-#svg(filename="barchart.svg", width=300, height=200, pointsize=14)
+png(filename="barchart.png", width=300, height=200, units="px", pointsize=14)
 par(mar=c(2,2,1,1))
 barplot(counts, col=c("darkgreen"))
+dev.off()
+
+
+png(filename="barchart2.png", width=300, height=200, units="px", pointsize=14)
+par(mar=c(2,2,1,1))
+barplot(counts, col=c("darkblue"), horiz=TRUE)
 dev.off()
