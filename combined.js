89d36e23bd27f267c796953a35e37bd25895dc5a
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-02-07 16:26:29 -0600

    Add pixel to data mapping for bar charts

--- combined.js
+++ combined.js
@@ -2184,22 +2184,53 @@ var wpd = wpd || {};
 wpd.BarAxes = (function () {
 
     var AxesObj = function () {
-        
+        // Throughout this code, it is assumed that "y" is the continuous axes and "x" is
+        // the discrete axes. In practice, this shouldn't matter.
+        var isCalibrated = false,
+            isLogScale = false,
+            x1, y1, x2, y2, p1, p2;
+
         this.isCalibrated = function () {
-            return true;
+            return isCalibrated;
         };
 
         this.calibrate = function(calibration, isLog) {
-            // TODO: use continuous axes coords to calculate a normal etc.
+            isCalibrated = false;
+            var cp1 = calibration.getPoint(0),
+                cp2 = calibration.getPoint(1);
+
+            x1 = cp1.px;
+            y1 = cp1.py;
+            x2 = cp2.px;
+            y2 = cp2.py;
+            p1 = cp1.dy;
+            p2 = cp2.dy;
+
+            if(isLog) {
+                isLogScale = true;
+                p1 = Math.log(p1)/Math.log(10);
+                p2 = Math.log(p2)/Math.log(10);
+            } else {
+                isLogScale = false;
+            }
+
+            isCalibrated = true;
             return true;
         };
 
         this.pixelToData = function (pxi, pyi) {
-            var data = [];
+            var data = [],
+                c_c2 = ((pyi-y1)*(y2-y1) + (x2-x1)*(pxi-x1))/((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+            data[0] = pxi;
+            data[1] = (p2 - p1)*c_c2;
+            if(isLogScale) {
+                data[1] = pow(10, data[1]);
+            }
             return data;
         };
 
         this.dataToPixel = function (x, y) {
+            // not implemented yet
             return {
                 x: 0,
                 y: 0
@@ -2207,7 +2238,12 @@ wpd.BarAxes = (function () {
         };
 
         this.pixelToLiveString = function (pxi, pyi) {
-            return "0";
+            var dataVal = this.pixelToData(pxi, pyi);
+            return dataVal[1].toExponential(4);
+        };
+
+        this.isLog = function () {
+            return isLogScale;
         };
 
         this.getTransformationEquations = function () {
