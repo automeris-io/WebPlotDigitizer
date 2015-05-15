ee0dd5e222bd96e922e843add33cc7a21050f44b
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-05-14 21:20:45 -0500

    add a simple WebSocket handler on the server side.

---
+++ server/websockethandler.py
@@ -0,0 +1,26 @@
+import tornado.websocket
+import uuid
+
+clients = dict()
+clientCount = 0
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    
+    def open(self, *args):
+        global clientCount
+        clientCount = clientCount + 1
+        print("Clients connected: %d" % clientCount)
+        self.id = str(uuid.uuid1())
+        self.stream.set_nodelay(True)
+        clients[self.id] = {"id": self.id, "object": self}
+
+    def on_message(self, message):
+        print(message)
+
+    def on_close(self):
+        print("socket closed")
+
+        if self.id in clients:
+            del clients[self.id]
+            global clientCount
+            clientCount = clientCount - 1
