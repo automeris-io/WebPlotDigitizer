a2f26d25f94d88a13f3709b771fcf8ec5ea195ae
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-05-14 19:35:03 -0500

    add missing files from previous commit and also add scaffolding for websocket communication layer

--- combined.js
+++ combined.js
@@ -8318,6 +8318,8 @@ wpd.ajax = (function() {
 
 */
 
+var wpd = wpd || {};
+
 wpd.args = (function() {
 
     // Simple argument parser
@@ -8828,6 +8830,58 @@ wpd.scriptInjector = (function () {
         cancel: cancel,
         load: load
     };
+})();
+/*
+	WebPlotDigitizer - http://arohatgi.info/WebPlotdigitizer
+
+	Copyright 2010-2015 Ankit Rohatgi <ankitrohatgi@hotmail.com>
+
+	This file is part of WebPlotDigitizer.
+
+    WebPlotDIgitizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebPlotDigitizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+var wpd = wpd || {};
+
+// Handle WebSocket Communication
+wpd.websocket = (function () {
+
+    function connect(address, port) {
+    }
+
+    function registerRequest() {
+    }
+
+    function registerNotification() {
+    }
+
+    function request() {
+    }
+
+    function notify() {
+    }
+
+    return {
+        connect: connect,
+        registerRequest: registerRequest,
+        registerNotification: registerNotification,
+        request: request,
+        notify: notify
+    };
+
 })();
 /*
 	WebPlotDigitizer - http://arohatgi.info/WebPlotdigitizer
---
+++ javascript/services/args.js
@@ -0,0 +1,53 @@
+/*
+	WebPlotDigitizer - http://arohatgi.info/WebPlotdigitizer
+
+	Copyright 2010-2015 Ankit Rohatgi <ankitrohatgi@hotmail.com>
+
+	This file is part of WebPlotDigitizer.
+
+    WebPlotDIgitizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebPlotDigitizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+var wpd = wpd || {};
+
+wpd.args = (function() {
+
+    // Simple argument parser
+    // e.g. 
+    // if WPD is launched as http://localhost:8000/index.html?q=1
+    // then getValue('q') should return '1'
+    // and getValue('nonexistent') should return null
+    function getValue(arg) {
+
+        var searchString = window.location.search.substring(1),
+            i,
+            val,
+            params = searchString.split("&");
+
+        for(i = 0; i < params.length; i++) {
+            val = params[i].split("=");
+            if(val[0] === arg) {
+                return unescape(val[1]);
+            }
+        }
+        return null;
+
+    }
+
+    return {
+        getValue: getValue
+    };
+})();
