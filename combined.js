6aacd5368587a303fff05ade5294b1f5fabe3721
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-06-07 11:44:13 -0500

    Show and error message if a non-image file is loaded into the app by accident. This fixes #48.

--- combined.js
+++ combined.js
@@ -4250,6 +4250,9 @@ wpd.graphicsWidget = (function () {
                 loadImageFromSrc(imageInfo);
             };
             droppedFile.readAsDataURL(fileInfo);
+        } else {
+            wpd.messagePopup.show("ERROR: Invalid File!", "Please load a valid image file. Common image formats such as JPG, PNG, BMP, GIF etc. should work. PDF or Word documents are not accepted.");
+            wpd.busyNote.close();
         }
     }
 
