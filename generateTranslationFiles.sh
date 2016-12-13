d277921acb30809e595a6abef6037c9a70ca8257
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2016-12-12 23:13:05 -0600

    add i18n support to html templates

---
+++ generateTranslationFiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pybabel -v extract -F templates/babel.config -o ./locale/messages.pot ./templates
+pybabel update -l en_US -d ./locale/ -i ./locale/messages.pot
+pybabel update -l fr_fr -d ./locale/ -i ./locale/messages.pot
+pybabel update -l zh_CN -d ./locale/ -i ./locale/messages.pot
