89d5f6e471f598aebeaaa83987ee2469d1ead2ba
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-01-09 21:18:34 -0600

    Add option to export all datasets at once. Bar graphs and date axes still need some work

--- combined.js
+++ combined.js
@@ -8822,6 +8822,107 @@ wpd.args = (function() {
     };
 })();
 /*
+	WebPlotDigitizer - http://arohatgi.info/WebPlotdigitizer
+
+	Copyright 2010-2016 Ankit Rohatgi <ankitrohatgi@hotmail.com>
+
+	This file is part of WebPlotDigitizer.
+
+    WebPlotDIgitizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebPlotDigitizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+var wpd = wpd || {};
+
+wpd.dataExport = (function () {
+
+    function show() {
+        // open dialog box explaining data format
+    }
+
+    function generateCSV() {
+        // generate file and trigger download
+
+        // loop over all datasets
+        var plotData = wpd.appData.getPlotData(),
+            axes = plotData.axes,
+            dsColl = plotData.dataSeriesColl,
+            i, j;
+
+        if(axes == null || dsColl == null || dsColl.length === 0) {
+            // axes is not aligned, show an error message?
+            return
+        }
+
+        var axLab = axes.getAxesLabels(),
+            axdims = axLab.length,
+            numCols = dsColl.length*axdims,
+            maxDatapts = 0,
+            pts,
+            header = [],
+            varheader = [],
+            valData = [];
+        
+        for(i = 0; i < dsColl.length; i++) {
+            pts = dsColl[i].getCount();
+            if(pts > maxDatapts) {
+                maxDatapts = pts;
+            }
+            header.push(dsColl[i].name);
+            for(j = 0; j < axdims; j++) {
+                if(j !== 0) {
+                    header.push('');
+                }
+                varheader.push(axLab[j]);
+            }
+        }
+        for(i = 0; i < maxDatapts; i++) {
+            var valRow = [];
+            for(j = 0; j < numCols; j++) {
+                valRow.push('');
+            }
+            valData.push(valRow);
+        }
+
+        for(i = 0; i < dsColl.length; i++) {
+            pts = dsColl[i].getCount();
+            for(j = 0; j < pts; j++) {
+                var px = dsColl[i].getPixel(j);
+                var val = axes.pixelToData(px.x, px.y);
+                var di;
+                for(di = 0; di < axdims; di++) {
+                    valData[j][i*axdims + di] = val[di];
+                }
+            }
+        }
+
+        var csvText = header.join(',') + '\n' + varheader.join(',') + '\n';
+        for(i = 0; i < maxDatapts; i++) {
+            csvText += valData[i].join(',') + '\n';
+        }
+        
+        // download
+        wpd.download.csv(JSON.stringify(csvText), "wpd_datasets");
+    }
+
+    return {
+        show: show,
+        generateCSV: generateCSV
+    };
+})();
+/*
 	WebPlotDigitizer - http://arohatgi.info/WebPlotDigitizer
 
 	Copyright 2010-2016 Ankit Rohatgi <ankitrohatgi@hotmail.com>
