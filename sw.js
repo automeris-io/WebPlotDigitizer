6dce66666b782ab289704265bf03603c4e877d32
Jake O'Shannessy <joshannessy@gmail.com>
2017-08-15 23:32:26 +1000

    Added service worker and manifest.

---
+++ sw.js
@@ -0,0 +1,67 @@
+// Service Worker
+var CACHE_NAME = 'wpd-cache-v1';
+// These are important urls that need to be cached on install.
+var urlsToCache = [
+  './',
+  './dev.html',
+  './images/icon192x192.png',
+  './images/icon512x512.png',
+  './manifest.json',
+  './styles.css',
+  './widgets.css',
+  './sw.js'
+];
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      }).catch((err) => {
+        console.error(err);
+        console.log(err);
+        throw new Error(err);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
