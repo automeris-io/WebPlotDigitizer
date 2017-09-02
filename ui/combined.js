d2e4230ab07b1bbbdae3994d2e0471239e2a12d2
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-09-02 18:19:09 -0500

    minor tweaks

--- ui/combined.js
+++ ui/combined.js
@@ -4659,8 +4659,8 @@ wpd.layoutManager = (function () {
         $sidebarControlsContainer.style.height = windowHeight - 280 + 'px';
         $mainContainer.style.width = windowWidth - $sidebarContainer.offsetWidth - 5 + 'px';
         $mainContainer.style.height = windowHeight + 'px';
-        $graphicsContainer.style.height = windowHeight - 44 + 'px';
-        $treeContainer.style.height = windowHeight - 44 + 'px';
+        $graphicsContainer.style.height = windowHeight - 45 + 'px';
+        $treeContainer.style.height = windowHeight - 45 + 'px';
         wpd.sidebar.resize();
     }
 
