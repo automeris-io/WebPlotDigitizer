2c57dd99f915f36374de6b9c633afbd36fa5c4b3
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-09-13 23:11:59 -0500

    Initial implementation of a json and image combined project file

--- ui/thirdparty/getThirdparty.sh
+++ ui/thirdparty/getThirdparty.sh
@@ -12,3 +12,6 @@ cd ..
 wget https://github.com/mozilla/pdf.js/releases/download/v1.8.188/pdfjs-1.8.188-dist.zip
 unzip pdfjs-1.8.188-dist.zip -d pdfjs
 
+# tarballjs
+wget https://github.com/ankitrohatgi/tarballjs/archive/master.zip
+unzip master.zip -d tarballjs
