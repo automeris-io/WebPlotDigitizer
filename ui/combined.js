3b756d03e137f24deb816e6ab2212936439b15e3
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-09-13 23:25:49 -0500

    clean up project save/resume behavior

--- ui/combined.js
+++ ui/combined.js
@@ -9394,9 +9394,7 @@ wpd.saveResume = (function () {
            for(i = 0; i < rdata.angleMeasurementData.length; i++) {
                plotData.angleMeasurementData.addConnection(rdata.angleMeasurementData[i]);
            }
-       }
-
-       wpd.tree.refresh();
+       }       
     }
 
     function generateJSON() {
@@ -9538,10 +9536,8 @@ wpd.saveResume = (function () {
             
             wpd.graphicsWidget.resetData();
             wpd.graphicsWidget.removeTool();
-            wpd.graphicsWidget.removeRepainter();
-            if(wpd.appData.isAligned()) {
-                wpd.acquireData.load();
-            }
+            wpd.graphicsWidget.removeRepainter();            
+            wpd.tree.refresh();
             wpd.messagePopup.show(wpd.gettext('import-json'), wpd.gettext("json-data-loaded"));
         };
         fileReader.readAsText(file);
@@ -9559,10 +9555,9 @@ wpd.saveResume = (function () {
                 wpdimage.name = "image.png";                
                 let wpdjson = JSON.parse(tarReader.getTextFile(projectName + "/wpd.json"));
                 wpd.imageManager.loadFromFile(wpdimage, true).then(() => {
-                    resumeFromJSON(wpdjson);                    
-                    if(wpd.appData.isAligned()) {
-                        wpd.acquireData.load();
-                    }                    
+                    resumeFromJSON(wpdjson);
+                    wpd.tree.refresh();
+                    wpd.messagePopup.show(wpd.gettext('import-json'), wpd.gettext("json-data-loaded"));
                 });
             }
         }, function(err) {
