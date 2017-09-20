a5a7803ba360676138aee649fe4c54b2a3b81b57
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-09-20 01:20:08 -0500

    begin work on adding multi-axes support

--- app/javascript/core/plotData.js
+++ app/javascript/core/plotData.js
@@ -24,71 +24,90 @@
 var wpd = wpd || {};
 
 // Plot information
-wpd.PlotData = (function () {
-    var PlotData = function() {
 
-        var activeSeriesIndex = 0,
-            autoDetector = new wpd.AutoDetector();
-        
+wpd.PlotData = class {
+    constructor() {
+        this.activeSeriesIndex = 0;
+        this.activeAxesIndex = 0;
+        this.autoDetector = new wpd.AutoDetector();
         this.topColors = null;
+        this.axesColl = [];
         this.axes = null;
         this.dataSeriesColl = [];
         this.gridData = null;
         this.calibration = null;
-
         this.angleMeasurementData = null;
         this.distanceMeasurementData = null;
         this.openPathMeasurementData = null;
         this.closedPathMeasurementData = null;
         this.backupImageData = null;
+        this.objectAxesMap = new Map();
+    }
 
-        this.getActiveDataSeries = function() {
-            if (this.dataSeriesColl[activeSeriesIndex] == null) {
-                this.dataSeriesColl[activeSeriesIndex] = new wpd.DataSeries();
-            }
-            return this.dataSeriesColl[activeSeriesIndex];
-        };
+    addAxes(ax) {
+        this.axesColl.push(ax);
+    }
 
-        this.getDataSeriesCount = function() {
-            return this.dataSeriesColl.length;
-        };
+    removeAxes(ax) {
+        this.axesColl = this.axesColl.filter(function(a) {
+            a.name !== ax.name;
+        });
+        
+        for(let k in this.objectAxesMap.keys()) {
+            this.objectAxesMap.set(k, null);
+        }
+    }
 
-        this.setActiveDataSeriesIndex = function(index) {
-            activeSeriesIndex = index;
-        };
+    getActiveAxes() {        
+        return this.axesColl[activeAxesIndex];
+    }
 
-        this.getActiveDataSeriesIndex = function() {
-            return activeSeriesIndex;
-        };
-
-        this.getAutoDetector = function() {
-            return autoDetector;
-        };
-
-        this.getDataSeriesNames = function() {
-            var rtnVal = [];
-            for(var i = 0; i < this.dataSeriesColl.length; i++) {
-                rtnVal[i] = this.dataSeriesColl[i].name;
-            }
-            return rtnVal;
-        };
-
-        this.reset = function() {
-            this.axes = null;
-            this.angleMeasurementData = null;
-            this.distanceMeasurementData = null;
-            this.openPathMeasurementData = null;
-            this.closedPathMeasurementData = null;
-            this.dataSeriesColl = [];
-            this.gridData = null;
-            this.calibration = null;
-            this.backupImageData = null;
-            activeSeriesIndex = 0;
-            autoDetector = new wpd.AutoDetector();
-        };
+    getActiveDataSeries() {
+        if (this.dataSeriesColl[activeSeriesIndex] == null) {
+            this.dataSeriesColl[activeSeriesIndex] = new wpd.DataSeries();
+        }
+        return this.dataSeriesColl[activeSeriesIndex];
     };
 
-    return PlotData;
-})();
+    getDataSeriesCount() {
+        return this.dataSeriesColl.length;
+    };
 
+    setActiveDataSeriesIndex(index) {
+        activeSeriesIndex = index;
+    };
+
+    getActiveDataSeriesIndex() {
+        return activeSeriesIndex;
+    };
+
+    getAutoDetector() {
+        return autoDetector;
+    };
+
+    getDataSeriesNames() {
+        var rtnVal = [];
+        for(var i = 0; i < this.dataSeriesColl.length; i++) {
+            rtnVal[i] = this.dataSeriesColl[i].name;
+        }
+        return rtnVal;
+    };
+
+    reset() {
+        this.axes = null;
+        this.axesColl = [];
+        this.objectAxesMap.clear();
+        this.angleMeasurementData = null;
+        this.distanceMeasurementData = null;
+        this.openPathMeasurementData = null;
+        this.closedPathMeasurementData = null;
+        this.dataSeriesColl = [];
+        this.gridData = null;
+        this.calibration = null;
+        this.backupImageData = null;
+        activeSeriesIndex = 0;
+        activeAxesIndex = 0;
+        autoDetector = new wpd.AutoDetector();
+    };
+};
 
