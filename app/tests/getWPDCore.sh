671640bdf1bc815911eb35bda18116347d8581ad
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-09-23 19:38:43 -0500

    Fix icon path for electron app and remove old tests code. New unit tests will be added soon

--- app/tests/getWPDCore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat ../javascript/core/*.js > wpdcore.js
-cat ../javascript/core/axes/*.js >> wpdcore.js
-cat ../javascript/core/AEalgos/*.js >> wpdcore.js
-cat wpdmodule.js >> wpdcore.js
