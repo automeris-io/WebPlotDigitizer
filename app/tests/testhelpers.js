99aed783de7b9cc6c111aca6b5a16205717aa250
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-10-15 22:42:41 -0500

    add test helpers

---
+++ app/tests/testhelpers.js
@@ -0,0 +1,29 @@
+var wpdtest = {};
+
+wpdtest.getBarAxes = function() {
+
+};
+
+wpdtest.getXYAxes = function() {
+
+};
+
+wpdtest.getPolarAxes = function() {
+
+};
+
+wpdtest.getTernaryAxes = function() {
+
+};
+
+wpdtest.getMapAxes = function() {
+
+};
+
+wpdtest.getImageAxes = function() {
+
+};
+
+wpdtest.getPlotData = function() {
+
+};
