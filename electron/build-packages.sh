ce25649b339f067ee9075633b7ec89583f57e5a4
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2017-10-30 19:29:37 -0500

    simplify menu bar

--- electron/build-packages.sh
+++ electron/build-packages.sh
@@ -11,11 +11,17 @@ APPNAME=WebPlotDigitizer
 # linux
 electron-packager ./electron $APPNAME --platform=linux --icon=app/images/icon/icon.png --arch=x64 --overwrite
 cp -r ./app $APPNAME-linux-x64/resources/
+rm -rf $APPNAME-linux-x64/resources/app/thirdparty/compiler-latest.zip
+rm -rf $APPNAME-linux-x64/resources/app/thirdparty/closure-compiler
 
 # windows
 electron-packager ./electron $APPNAME --platform=win32 --icon=app/images/icon/icon.ico --arch=x64 --overwrite
 cp -r ./app $APPNAME-win32-x64/resources/
+rm -rf $APPNAME-win32-x64/resources/app/thirdparty/compiler-latest.zip
+rm -rf $APPNAME-win32-x64/resources/app/thirdparty/closure-compiler
 
 # mac
 electron-packager ./electron $APPNAME --platform=darwin --icon=app/images/icon/wpd.icns --arch=x64 --overwrite
 cp -r ./app $APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/
+rm -rf $APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/app/thirdparty/compiler-latest.zip
+rm -rf $APPNAME-darwin-x64/$APPNAME.app/Contents/Resources/app/thirdparty/closure-compiler
