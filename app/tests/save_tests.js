250d7ab0f457f98746366ab18097e35405a3dcda
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2018-05-07 00:34:52 -0500

    Start working on save/resume unit tests

--- app/tests/save_tests.js
+++ app/tests/save_tests.js
@@ -1,13 +1,32 @@
 QUnit.module("Save/Resume Tests");
 QUnit.test("Resume Version 3.x JSON", function(assert) {
-    assert.ok(true);
+    let done = assert.async();
+    wpdtest.fetchJSON("files/wpd3_xy.json").then(data => {
+        assert.equal(data.wpd.version[0], 3, "data has version 3");
+        let plotData = new wpd.PlotData();
+        plotData.deserialize(data);
+        
+        // start verifying data
+        assert.equal(plotData.getAxesCount(), 1, "One axes calibration loaded");
+        done();
+    });    
 });
 
 QUnit.test("Resume Version 4 JSON", function(assert) {
-    assert.ok(true);
+    let done = assert.async();
+    wpdtest.fetchJSON("files/wpd4.json").then(data => {
+        assert.equal(data.version[0], 4, "data has version 4");
+        let plotData = new wpd.PlotData();
+        plotData.deserialize(data);
+
+        // start verifying data
+        assert.equal(plotData.getAxesCount(), 6, "Six axes calibrations loaded");
+        done();
+    });
 });
 
 QUnit.test("Save Version 4 JSON", function(assert) {
+    // resume, then save, then resume again?
     assert.ok(true);
 });
 
