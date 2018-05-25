cf0fd8e6a6087f19430ea1e42ce7d26b305ae7a4
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2018-05-25 00:06:52 -0500

    Remove unnecessary closure

--- app/javascript/tools/keyCodes.js
+++ app/javascript/tools/keyCodes.js
@@ -23,40 +23,38 @@
 
 var wpd = wpd || {};
 
-wpd.keyCodes = (function () {
-    return {
-        isUp: function(code) {
-            return code === 38;
-        },
-        isDown: function(code) {
-            return code === 40;
-        },
-        isLeft: function(code) {
-            return code === 37;
-        },
-        isRight: function(code) {
-            return code === 39;
-        },
-        isTab: function(code) {
-            return code === 9;
-        },
-        isDel: function(code) {
-            return code === 46;
-        },
-        isBackspace: function(code) {
-            return code === 8;
-        },
-        isAlphabet: function(code, alpha) {
-            if (code > 90 || code < 65) {
-                return false;
-            }
-            return String.fromCharCode(code).toLowerCase() === alpha;
-        },
-        isEnter: function(code) {
-            return code === 13;
-        },
-        isEsc: function(code) {
-            return code === 27;
+wpd.keyCodes = {
+    isUp: function(code) {
+        return code === 38;
+    },
+    isDown: function(code) {
+        return code === 40;
+    },
+    isLeft: function(code) {
+        return code === 37;
+    },
+    isRight: function(code) {
+        return code === 39;
+    },
+    isTab: function(code) {
+        return code === 9;
+    },
+    isDel: function(code) {
+        return code === 46;
+    },
+    isBackspace: function(code) {
+        return code === 8;
+    },
+    isAlphabet: function(code, alpha) {
+        if (code > 90 || code < 65) {
+            return false;
         }
-    };
-})();
+        return String.fromCharCode(code).toLowerCase() === alpha;
+    },
+    isEnter: function(code) {
+        return code === 13;
+    },
+    isEsc: function(code) {
+        return code === 27;
+    }
+};
