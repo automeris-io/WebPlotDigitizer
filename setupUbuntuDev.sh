2029fc5ff64391a66dd87a2ca5776425dee12ab1
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2018-12-28 23:57:56 -0800

    Add script to setup an Ubuntu based development machine for WPD development

---
+++ setupUbuntuDev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setup an Ubuntu system for development
+
+# install Ubuntu packages
+sudo apt install python3-jinja2 python3-babel wine npm default-jre
+
+# install global npm packages
+sudo npm install -g electron-packager js-beautify
+
+# get other dependencies
+cd electron
+npm install
+cd ..
+
+cd app/thirdparty
+./getThirdparty.sh
+cd ../..
+
+
