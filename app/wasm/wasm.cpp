f5a134cb59cfa302b99a37332446ce5c4a1356b7
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-01-04 01:14:22 -0800

    Export a test function from C++ to WASM

--- app/wasm/wasm.cpp
+++ app/wasm/wasm.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
 #include <Eigen/Core>
+#include <emscripten.h>
+#include <iostream>
 
-int main() {
-    Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
-    std::cout << mat << std::endl;
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    int testfn() {
+        Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
+        std::cout << mat << std::endl;
+        return 0;
+    }
 }
