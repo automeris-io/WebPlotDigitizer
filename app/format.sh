09523b0737e0760897f38eee1e3f4d04081d674c
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-01-06 20:37:44 -0800

    Include WebAssembly code when autoformatting

--- app/format.sh
+++ app/format.sh
@@ -12,4 +12,9 @@ find templates -name "*.html" -exec js-beautify -r {} \;
 echo " - CSS styles"
 find . -name "*.css" -exec js-beautify -r {} \;
 
+echo " - WebAssembly C++"
+cd wasm
+./format.sh
+cd ..
+
 echo "Done!"
--- app/wasm/wasm.cpp
+++ app/wasm/wasm.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 
 extern "C" {
-    EMSCRIPTEN_KEEPALIVE
-    int testfn() {
-        Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
-        std::cout << mat << std::endl;
-        return 0;
-    }
+EMSCRIPTEN_KEEPALIVE
+int testfn() {
+    Eigen::Matrix4d mat = Eigen::Matrix4d::Identity();
+    std::cout << mat << std::endl;
+    return 0;
+}
 }
