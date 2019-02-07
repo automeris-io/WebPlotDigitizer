6ac1d1bed1da637b83e4b583f374f66127803a12
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-02-06 20:42:43 -0800

    Auto-format headers

--- app/wasm/helpers.h
+++ app/wasm/helpers.h
@@ -2,10 +2,10 @@
 #include <Eigen/Core>
 
 namespace WPD {
-    struct Helpers {
-        static Eigen::Matrix3d computeHomography(Eigen::Vector2d source[4], Eigen::Vector2d target[4]) {
-            // TODO: Assemble Ah = 0, perform SVD and return solution
-            return Eigen::Matrix3d::Identity();
-        }
-    };
-}
+struct Helpers {
+    static Eigen::Matrix3d computeHomography(Eigen::Vector2d source[4], Eigen::Vector2d target[4]) {
+        // TODO: Assemble Ah = 0, perform SVD and return solution
+        return Eigen::Matrix3d::Identity();
+    }
+};
+} // namespace WPD
