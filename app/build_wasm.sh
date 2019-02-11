215f2aade0c4c2969b1b6394dbbc9f704cf4ce5c
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-02-10 18:29:16 -0800

    Add -O3 optimization for WebAssembly compilation

--- app/build_wasm.sh
+++ app/build_wasm.sh
@@ -4,4 +4,4 @@
 source thirdparty/emsdk/emsdk_env.sh > debug.out
 
 # build wasm
-em++ -std=c++17 wasm/*.cpp -I /usr/include/eigen3 -o wasm.js
+em++ -std=c++17 -O3 wasm/*.cpp -I /usr/include/eigen3 -o wasm.js
