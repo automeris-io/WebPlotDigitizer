a7beb1eb490997cb32b2f914aa1f54fdda3648fa
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-02-24 22:25:21 -0800

    Implement image cropping

--- app/javascript/controllers/imageEditing.js
+++ app/javascript/controllers/imageEditing.js
@@ -16,7 +16,8 @@
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.*/
+    along with WebPlotDigitizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var wpd = wpd || {};
 
@@ -62,22 +63,49 @@ wpd.CropImageAction = class extends wpd.ReversibleAction {
         this._y0 = y0;
         this._x1 = x1;
         this._y1 = y1;
+        this._originalImage = null;
     }
 
     execute() {
         // store current image for undo
+        let ctx = wpd.graphicsWidget.getAllContexts();
+        let imageSize = wpd.graphicsWidget.getImageSize();
+        this._originalImage = ctx.oriImageCtx.getImageData(0, 0, imageSize.width, imageSize.height);
 
         // crop image
+        let croppedImage = ctx.oriImageCtx.getImageData(this._x0, this._y0, this._x1, this._y1);
+        let croppedWidth = this._x1 - this._x0;
+        let croppedHeight = this._y1 - this._y0;
 
         // replace current image with cropped image
+        let imageOp = function(imageData, width, height) {
+            return {
+                imageData: croppedImage,
+                width: croppedWidth,
+                height: croppedHeight,
+                keepZoom: true
+            };
+        };
 
-        // call all dependent UI actions
-        console.log("crop action called!");
+        wpd.graphicsWidget.removeRepainter();
+        wpd.graphicsWidget.resetData();
+        wpd.graphicsWidget.runImageOp(imageOp);
     }
 
     undo() {
         // set the saved image
+        let originalImage = this._originalImage;
+        let imageOp = function(imageData, width, height) {
+            return {
+                imageData: originalImage,
+                width: originalImage.width,
+                height: originalImage.height
+            };
+        };
 
         // call all dependent UI elements
+        wpd.graphicsWidget.removeRepainter();
+        wpd.graphicsWidget.resetData();
+        wpd.graphicsWidget.runImageOp(imageOp);
     }
 };
