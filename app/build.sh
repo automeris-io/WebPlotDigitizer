e0d89b54b7a3af3d8c29593b5aa86f533530029c
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-02-24 22:29:38 -0800

    Split build script for faster iteration

--- app/build.sh
+++ app/build.sh
@@ -3,27 +3,11 @@
 echo "Compiling WebAssembly Code..."
 ./build_wasm.sh
 
-echo "Combining Javascript Files..."
-cat javascript/*.js > combined.js
-cat javascript/core/*.js >> combined.js
-cat javascript/core/AEalgos/*.js >> combined.js
-cat javascript/core/axes/*.js >> combined.js
-cat javascript/widgets/*.js >> combined.js
-cat javascript/tools/*.js >> combined.js
-cat javascript/services/*.js >> combined.js
-cat javascript/controllers/*.js >> combined.js
-
-echo "Compiling Javascript Files..."
-java -jar thirdparty/closure-compiler/compiler.jar --js combined.js --js_output_file combined-compiled.js
+echo "Compiling Javascript Code..."
+./build_js.sh
 
 echo "Update translation files..."
-pybabel -v extract -F templates/babel.config -o ./locale/messages.pot ./templates
-pybabel update -l en_US -d ./locale/ -i ./locale/messages.pot
-pybabel update -l fr_FR -d ./locale/ -i ./locale/messages.pot
-pybabel update -l zh_CN -d ./locale/ -i ./locale/messages.pot
-pybabel update -l de_DE -d ./locale/ -i ./locale/messages.pot
-pybabel update -l ja -d ./locale/ -i ./locale/messages.pot
-pybabel update -l ru -d ./locale/ -i ./locale/messages.pot
+./build_translations.js
 
 echo "Rendering HTML Pages..."
 python3 renderHTML.py
---
+++ app/build_js.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat javascript/*.js > combined.js
+cat javascript/core/*.js >> combined.js
+cat javascript/core/AEalgos/*.js >> combined.js
+cat javascript/core/axes/*.js >> combined.js
+cat javascript/widgets/*.js >> combined.js
+cat javascript/tools/*.js >> combined.js
+cat javascript/services/*.js >> combined.js
+cat javascript/controllers/*.js >> combined.js
+
+# compile using closure compiler
+java -jar thirdparty/closure-compiler/compiler.jar --js combined.js --js_output_file combined-compiled.js
+
