b54c1cfbf36bbf7f8e3c91bba78b7b5076295847
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-03-07 21:22:59 -0800

    Implement resizing crop box

--- app/javascript/tools/imageEditingTools.js
+++ app/javascript/tools/imageEditingTools.js
@@ -26,12 +26,18 @@ wpd.CropTool = class {
     constructor() {
         this._isDrawing = false;
         this._hasCropBox = false;
+        this._isResizing = false;
         this._topImageCorner = null;
         this._topScreenCorner = null;
         this._moveTimer = null;
         this._screenPos = null;
         this._imagePos = null;
         this._hotspotCoords = null;
+        this._resizingHotspot = '';
+        this._resizeStartCoords = {
+            x: 0,
+            y: 0
+        };
         this._ctx = wpd.graphicsWidget.getAllContexts();
     }
 
@@ -53,7 +59,12 @@ wpd.CropTool = class {
             let hotspot = this._getHotspot(pos);
             if (hotspot != null) {
                 // initiate resize/move action
-                console.log(hotspot);
+                this._isResizing = true;
+                this._resizeStartCoords = {
+                    x: pos.x,
+                    y: pos.y
+                };
+                this._resizingHotspot = hotspot;
             }
         }
     }
@@ -68,7 +79,7 @@ wpd.CropTool = class {
             }, 2);
         } else if (this._hasCropBox) {
             // reposition selected point (and others to match)
-            let hotspot = this._getHotspot(pos);
+            let hotspot = this._isResizing ? this._resizingHotspot : this._getHotspot(pos);
 
             // set the appropriate cursor on hover or resize
             if (hotspot != null) {
@@ -90,6 +101,48 @@ wpd.CropTool = class {
             }
 
             // resize or move based on hotspot
+            if (this._isResizing) {
+                let posDiff = {
+                    x: pos.x - this._resizeStartCoords.x,
+                    y: pos.y - this._resizeStartCoords.y
+                };
+                if (this._resizingHotspot == "n") {
+                    this._topScreenCorner.y += posDiff.y;
+                } else if (this._resizingHotspot == "s") {
+                    this._screenPos.y += posDiff.y;
+                } else if (this._resizingHotspot == "w") {
+                    this._topScreenCorner.x += posDiff.x;
+                } else if (this._resizingHotspot == "e") {
+                    this._screenPos.x += posDiff.x;
+                } else if (this._resizingHotspot == "nw") {
+                    this._topScreenCorner.y += posDiff.y;
+                    this._topScreenCorner.x += posDiff.x;
+                } else if (this._resizingHotspot == "ne") {
+                    this._topScreenCorner.y += posDiff.y;
+                    this._screenPos.x += posDiff.x;
+                } else if (this._resizingHotspot == "sw") {
+                    this._screenPos.y += posDiff.y;
+                    this._topScreenCorner.x += posDiff.x;
+                } else if (this._resizingHotspot == "se") {
+                    this._screenPos.y += posDiff.y;
+                    this._screenPos.x += posDiff.x;
+                } else if (this._resizingHotspot == "c") {
+                    this._topScreenCorner.x += posDiff.x;
+                    this._topScreenCorner.y += posDiff.y;
+                    this._screenPos.x += posDiff.x;
+                    this._screenPos.y += posDiff.y;
+                }
+
+                clearTimeout(this._moveTimer);
+                this._moveTimer = setTimeout(() => {
+                    this._drawCropBox();
+                }, 2);
+
+                this._resizeStartCoords = {
+                    x: pos.x,
+                    y: pos.y
+                };
+            }
         }
     }
 
@@ -120,10 +173,11 @@ wpd.CropTool = class {
 
     _finalizeDrawing() {
         clearTimeout(this._moveTimer);
-        if (!this._isDrawing)
+        if (!this._isDrawing && !this._isResizing)
             return;
 
         this._isDrawing = false;
+        this._isResizing = false;
         this._hasCropBox = true;
         this._drawCropBox();
     }
