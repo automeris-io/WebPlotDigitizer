585bc3dc56b5b974392fdd40c1729f19aafeb7be
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-04-06 20:25:41 -0700

    Clean up Node module documentation, also package the module in a easy to upload manner

--- app/build_node_module.sh
+++ app/build_node_module.sh
@@ -11,3 +11,12 @@ printf "\nlet Module = require(\"./wasm.js\");\n" >> wpd_node.js
 # Export Module
 printf "\nmodule.exports = { wpd: wpd };\n" >> wpd_node.js
 
+# Package Module
+
+VERSION=4.2
+mkdir -p wpd-$VERSION/
+cp wpd_node.js wpd-$VERSION/
+cp wasm.js wpd-$VERSION/
+cp wasm.wasm wpd-$VERSION/
+tar -czf wpd-$VERSION.tar.gz wpd-$VERSION
+
