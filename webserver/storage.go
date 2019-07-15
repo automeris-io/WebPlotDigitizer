d39b9b3b64d86b3d523436c33f6ec7e68752f164
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2019-07-14 21:31:25 -0700

    Harden storage backend handler

--- webserver/storage.go
+++ webserver/storage.go
@@ -7,17 +7,29 @@ import (
 
 // Storage - handle data storage
 type Storage struct {
-	path string
+	enabled bool
+	path    string
 }
 
 // InitStorage - initialize storage
 func InitStorage(settings *ServerSettings) (*Storage, error) {
-	storage := Storage{path: settings.Storage.Path}
+	storage := Storage{path: settings.Storage.Path, enabled: settings.Storage.Enabled}
 	return &storage, nil
 }
 
 // ServeHTTP - HTTP request handler
 func (s *Storage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// only proceed if enabled
+	if !s.enabled {
+		return
+	}
+
+	// only allow GET requests
+	if r.Method != "GET" {
+		return
+	}
+
+	// Path has to follow correct pattern
 	cleanURL := path.Clean(r.URL.Path)
 	matches, err := path.Match("/storage/project/*.tar", cleanURL)
 	if err != nil {
