6f02d9927406fd88bf12787bbccf7237c16a06e3
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2021-09-01 23:32:14 -0700

    add scaffolding for circular chart recorder

---
+++ app/tests/circular_chart_recorder_tests.js
@@ -0,0 +1,11 @@
+QUnit.module("Circular chart recorder tests");
+QUnit.test("Calibration", function(assert) {
+
+    let calib = new wpd.Calibration(2);
+    // add 5 calibration points
+
+    let circAxes = wpd.CircularChartRecorder();
+    circAxes.calibrate(calib);
+    
+    assert.equal(1,1, "dummy");
+});
